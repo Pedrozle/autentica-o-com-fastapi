f8ff1d9f161b3bf617d911f58b40d37b620d955f
Pedrozle <pedrozle@outlook.com>
2023-03-05 13:01:30 -0400

    test 1 - no setup build

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name="services", packages=['src/services'])
-setup(name="db", packages=['src/db'])
-setup(name="models", packages=['src/models'])
+setup(name="src", packages=find_packages())
